f70b5d48b6eef2b1d4369e14aea9b27f36e5c5de
AdryanoJuniorQA <130603192+AdryanoJunior@users.noreply.github.com>
2024-04-18 13:00:35 -0300

    Add files via upload

--- mdc.test.js
+++ mdc.test.js
@@ -1,3 +1,15 @@
-const multiplicação = (a, b) => a * b
+const { mdc } = require('./mdc');
 
-module.exports = { multiplicação };
+describe('Teste da função mdc', () => {
+    it('deve retornar o MDC de dois números', () => {
+        expect(mdc(72, 108)).toBe(36);
+        expect(mdc(12, 28)).toBe(4);
+        expect(mdc(18, 60)).toBe(6);
+    });
+
+    it('deve retornar o próprio número se um dos números for zero', () => {
+        expect(mdc(0, 17)).toBe(5);
+        expect(mdc(39, 0)).toBe(10);
+        expect(mdc(0, 0)).toBe(0);
+    });
+});
